c1805c8a483776f66a03d625ab8591fe97e893cc
Barathkumar2003 <164003950+Barathkumar2003@users.noreply.github.com>
2024-05-03 12:59:06 +0530

    Add files via upload

---
+++ RatingUI/interactive-rating-component-main/script.js
@@ -0,0 +1,10 @@
+const submit = document.getElementById('submit');
+const ratingNumber = document.querySelector('.ratings')
+
+function submitted() {
+
+}
+
+function rating() {
+  ratingNumber.style.backgroundColor = lightgray;
+}
